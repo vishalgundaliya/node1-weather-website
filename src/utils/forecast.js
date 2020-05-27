ccefb6717a213c6982ef0b0f9444d47141d4b9cd
vishal <gundalia@gmail.com>
2020-05-27 21:46:39 +0530

    Add feellikes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,13 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=ccad5b72dcfbdbae114863a75af6f057&query=' + latitude + ',' + longitude
-
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'Weather is ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' c degree outside');
+            callback(undefined, 'Weather is ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' c degree outside.' + ' It is feels like ' + body.current.feelslike + ".");
         }
     })
 }
